f4d5901a3e0cd6d3cc7035f38f9fd5944f1e3939
Alice Heaton <aliceh@aptivate.org>
2016-11-10 20:06:49 +0100

    Add second level nav menu styling

--- wordpress/wp-content/themes/mapaction/header.php
+++ wordpress/wp-content/themes/mapaction/header.php
@@ -44,7 +44,12 @@
 		</div><!-- .site-branding -->
 		<nav id="site-navigation" class="main-navigation main-navigation-layout" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'mapaction' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+			<?php wp_nav_menu( array(
+				'container' => false,
+				'theme_location' => 'primary',
+				'menu_id' => 'primary-menu',
+				) );
+			?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
