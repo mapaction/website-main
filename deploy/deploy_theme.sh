3b91d2e4b5c384caccc4bddefede229f9a23655d
Alice Heaton <aliceh@aptivate.org>
2016-11-14 13:12:36 +0100

    Shell script to deploy theme. WIP, may require some fixes.

---
+++ deploy/deploy_theme.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DEPLOY_DIR=`dirname $0`
+ROOT=`readlink -f $DEPLOY_DIR/..`
+WP_ROOT="$ROOT/wordpress"
+
+HOST='sftp.flywheelsites.com'
+REMOTE_WP_ROOT='/org-mapaction/mapactionmain'
+PATH_TO_THEME_FOLDER='wp-content/themes'
+THEME_NAME='mapaction'
+
+NOW=`date +"%Y-%m-%d_%H-%M"`
+
+echo -n "Please enter your username for $HOST: "
+read USERNAME
+
+sftp "$USERNAME@$HOST" <<END
+    rename "$REMOTE_WP_ROOT/$PATH_TO_THEME_FOLDER/$THEME_NAME" "$REMOTE_WP_ROOT/${THEME_NAME}_$NOW.bak"
+    mkdir "$REMOTE_WP_ROOT/$PATH_TO_THEME_FOLDER/$THEME_NAME"
+    put -r "$WP_ROOT/$PATH_TO_THEME_FOLDER/$THEME_NAME" "$REMOTE_WP_ROOT/$PATH_TO_THEME_FOLDER"
+END
