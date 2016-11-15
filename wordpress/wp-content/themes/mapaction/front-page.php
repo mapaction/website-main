0295f014e30d34f9a3fd4f8315482a15a5975c8b
Alice Heaton <aliceh@aptivate.org>
2016-11-15 12:25:03 +0100

    Implement front page
    Implement the front page as a widget container where they can drag widgets in the WP interface, and implement the responsive layout for it.

---
+++ wordpress/wp-content/themes/mapaction/front-page.php
@@ -0,0 +1,12 @@
+<?php
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main dashboard dashboard-layout" role="main">
+			<?php dynamic_sidebar('front-page'); ?>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
