16f8c64616325cfb29c132abf5f31b954349a112
Alice Heaton <aliceh@aptivate.org>
2016-11-25 16:14:22 +0100

    Change the posted on format so that it only displays the date (not as a link), and not the author.

--- wordpress/wp-content/themes/mapaction/inc/template-tags.php
+++ wordpress/wp-content/themes/mapaction/inc/template-tags.php
@@ -13,10 +13,6 @@ if ( ! function_exists( 'mapaction_posted_on' ) ) :
  */
 function mapaction_posted_on() {
 	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-	}
-
 	$time_string = sprintf( $time_string,
 		esc_attr( get_the_date( 'c' ) ),
 		esc_html( get_the_date() ),
@@ -26,15 +22,10 @@ function mapaction_posted_on() {
 
 	$posted_on = sprintf(
 		esc_html_x( 'Posted on %s', 'post date', 'mapaction' ),
-		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+		$time_string
 	);
 
-	$byline = sprintf(
-		esc_html_x( 'by %s', 'post author', 'mapaction' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-	);
-
-	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+	echo '<span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
 
 }
 endif;
