9fd2454272d8e5d884f3407c7d09c82826769258
Alice Heaton <aliceh@aptivate.org>
2016-11-25 15:25:11 +0100

    Make it possible to pipe/redirect into the mysql.sh script

--- dev/bin/mysql.sh
+++ dev/bin/mysql.sh
@@ -3,4 +3,10 @@
 BIN_DIR=`dirname $0`
 ROOT=`readlink -f $BIN_DIR/../..`
 
-docker exec -t -i mapaction-mysql mysql -u mapaction -pmapaction mapaction
+# Test for interactive shell
+if [ -t 0 ]; then
+	OPTS="-t -i"
+else
+	OPTS="-i"
+fi
+docker exec $OPTS mapaction-mysql mysql -u mapaction -pmapaction mapaction
