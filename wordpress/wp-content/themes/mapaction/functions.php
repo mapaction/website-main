27e13d6dacb68e49b5ee24776f41f3f202c799f9
Alice Heaton <aliceh@aptivate.org>
2016-11-25 17:54:49 +0100

    Implement category listing pages (eg. news)
    - Custom navigation sidebar
    - Styling
    - Use exceprts, not whole posts

--- wordpress/wp-content/themes/mapaction/functions.php
+++ wordpress/wp-content/themes/mapaction/functions.php
@@ -98,6 +98,16 @@ function mapaction_widgets_init() {
 	) );
 
 	register_sidebar( array(
+		'name'          => esc_html__( 'Category Sidebar', 'mapaction' ),
+		'id'            => 'category-sidebar',
+		'description'   => esc_html__( 'Add widgets to the sidebar on category listing pages', 'mapaction' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+
+	register_sidebar( array(
 		'name'          => esc_html__( 'Footer', 'mapaction' ),
 		'id'            => 'footer',
 		'description'   => esc_html__( 'Add widgets to the footer area', 'mapaction' ),
